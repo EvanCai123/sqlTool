733f7f5ae54f4d6a6f892e6b6f99cdb673ed0c53
unknown <afra.liu@afra-liu.achievo.com>
2014-06-12 03:10:16 +0800

    For Auto Deploy Tool

---
+++ checkVersion.bat
@@ -0,0 +1,12 @@
+echo Failed>result.txt
+cd ..
+set JAVA_HOME=%CD%\JDK1.7
+set PATH=.;%JAVA_HOME%\bin
+cd util
+set CLASSPATH=%CD%\lib\autoDeployTool.jar;%CD%\lib\sqljdbc4.jar D:\sourcecode\util\version.txt 
+echo %JAVA_HOME%
+echo %PATH%
+echo %CLASSPATH%
+
+java com.accela.version.util.CheckVersion %CD%\version.txt %1
+pause
