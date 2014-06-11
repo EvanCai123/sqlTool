0f711e98702f87f59bbd02a06fba7224552082c0
unknown <afra.liu@afra-liu.achievo.com>
2014-06-12 03:50:10 +0800

    Auto Run SQL File

--- checkVersion.bat
+++ checkVersion.bat
@@ -3,10 +3,10 @@ cd ..
 set JAVA_HOME=%CD%\JDK1.7
 set PATH=.;%JAVA_HOME%\bin
 cd util
-set CLASSPATH=%CD%\lib\autoDeployTool.jar;%CD%\lib\sqljdbc4.jar D:\sourcecode\util\version.txt 
+set CLASSPATH=%CD%\lib\autoDeployTool.jar;%CD%\lib\sqljdbc4.jar
 echo %JAVA_HOME%
 echo %PATH%
 echo %CLASSPATH%
 
-java com.accela.version.util.CheckVersion "%CD%\version.txt" %1
+java com.accela.version.util.CheckVersion %CD%\version.txt %1
 pause
--- runSql.bat
+++ runSql.bat
@@ -1,1 +1,11 @@
 echo Failed>result.txt
+cd ..
+set JAVA_HOME=%CD%\JDK1.7
+set PATH=.;%JAVA_HOME%\bin
+cd util
+set CLASSPATH=%CD%\lib\autoDeployTool.jar;%CD%\lib\sqljdbc4.jar;%CD%\lib\ant.jar
+echo %JAVA_HOME%
+echo %PATH%
+echo %CLASSPATH%
+
+java com.accela.sql.util.ExcuteSQL %1 %2 %3
