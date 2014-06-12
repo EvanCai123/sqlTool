8370e42605e526764a2a1af788158f1236fa7939
unknown <afra.liu@afra-liu.achievo.com>
2014-06-12 13:26:15 +0800

    Keep PATH and CLASSPATH value

--- checkVersion.bat
+++ checkVersion.bat
@@ -1,12 +1,8 @@
 echo Failed>result.txt
 cd ..
 set JAVA_HOME=%CD%\JDK1.7
-set PATH=.;%JAVA_HOME%\bin
+set PATH=%PATH%;%JAVA_HOME%\bin
 cd util
-set CLASSPATH=%CD%\lib\autoDeployTool.jar;%CD%\lib\sqljdbc4.jar
-echo %JAVA_HOME%
-echo %PATH%
-echo %CLASSPATH%
+set CLASSPATH=%CLASSPATH%;%CD%\lib\autoDeployTool.jar;%CD%\lib\sqljdbc4.jar
 
 java com.accela.version.util.CheckVersion "%CD%\version.txt" %1
-pause
--- runSql.bat
+++ runSql.bat
@@ -1,11 +1,9 @@
 echo Failed>result.txt
 cd ..
 set JAVA_HOME=%CD%\JDK1.7
-set PATH=.;%JAVA_HOME%\bin
+set PATH=%PATH%;%JAVA_HOME%\bin
 cd util
-set CLASSPATH=%CD%\lib\autoDeployTool.jar;%CD%\lib\sqljdbc4.jar;%CD%\lib\ant.jar
-echo %JAVA_HOME%
-echo %PATH%
-echo %CLASSPATH%
+set CLASSPATH=%CLASSPATH%;%CD%\lib\autoDeployTool.jar;%CD%\lib\sqljdbc4.jar;%CD%\lib\ant.jar
+
 
 java com.accela.sql.util.ExcuteSQL %1 %2 %3
