897e9408827ad68fbea754eba4ebeacbc3ee67a3
unknown <afra.liu@afra-liu.achievo.com>
2014-06-13 20:49:08 +0800

    Check Version

--- checkVersion.bat
+++ checkVersion.bat
@@ -1,8 +1,8 @@
 echo Failed>result.txt
 cd ..
 set JAVA_HOME=%CD%\JDK1.7
-set PATH=%PATH%;%JAVA_HOME%\bin
+set PATH=%JAVA_HOME%\bin
 cd util
-set CLASSPATH=%CLASSPATH%;%CD%\lib\autoDeployTool.jar;%CD%\lib\sqljdbc4.jar
+set CLASSPATH=%CD%\lib\autoDeployTool.jar;%CD%\lib\sqljdbc4.jar
 
 java com.accela.version.util.CheckVersion "%CD%\version.txt" %1
